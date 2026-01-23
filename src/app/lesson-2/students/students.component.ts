22396b36e25a0dff9f0aba6e150e8ba9e3f0def3
Abdurizo <103021939+abdurizo@users.noreply.github.com>
2026-01-23 12:18:33 +0500

    id fix

--- src/app/lesson-2/students/students.component.ts
+++ src/app/lesson-2/students/students.component.ts
@@ -5,6 +5,7 @@ import { ButtonHomeComponent } from '../../button-home/button-home.component';
 
 
 interface Student {
+  id:number;
   name: string;
   score: number;
 }
@@ -20,16 +21,21 @@ export class StudentsComponent {
   students: Student[] = [];
   name = '';
   score: number | null = null;
+  private nextId:number = 1;
   /***
    */
   addStudent() {
     if (!this.name || this.score === null) return;
     this.students.push({
+      id: this.nextId++,
       name: this.name,
       score: this.score!,
     });
     this.name = '';
     this.score = null;
+  }
+  deleteStudent(id:number){
+this.students = this.students.filter(student => student.id !== id)
   }
   getColor(score: number): string {
     if (score >= 90) return '#4cdb3ff1';
